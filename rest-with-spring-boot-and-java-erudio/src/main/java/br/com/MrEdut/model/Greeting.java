1c517a22253d350bea362b750f71c08918563d05
MrEdut <kdufs@protonmail.com>
2025-04-02 23:38:49 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/MrEdut/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.MrEdut.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/MrEdut/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.MrEdut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
